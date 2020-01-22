74eb9f921ac542c1d2ba67f52ded348ce7ee2655
Malik-shahzadAli <shahzadali@dedevtech.com>
2020-01-22 18:19:30 +0500

    validatename

--- src/app/views/pages/forms/form.validator.ts
+++ src/app/views/pages/forms/form.validator.ts
@@ -6,17 +6,17 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class FileNameValidator{
     URL=CommonClass.commonUrl;
     http: HttpClient;
-    token= localStorage.getItem('token');
-    head = {
-         headers: new HttpHeaders().append('Authorization', `Bearer ${this.token}`),
-        }
+
     //when we want to get result from server and want to 
     //await for asynochronous call we should use the Promise
     shouldUnique(control:AbstractControl): Promise< ValidationErrors |null > {
-    
+        let token= localStorage.getItem('token');
+        let  head = {
+         headers: new HttpHeaders().append('Authorization', `Bearer ${token}`),
+        }
         return new Promise((resolve,reject)=>{
 
-        this.http.post(this.URL+'files/filename',control.value,this.head)
+        this.http.post(this.URL+'files/filename',control.value,head)
 		.subscribe(
             res => resolve(null),
             error=>resolve({shouldUnique:true})
