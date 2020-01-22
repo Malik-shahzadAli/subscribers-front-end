9b8609c6131ae37af44a6d0ddb1323a374fef79f
Malik-shahzadAli <shahzadali@dedevtech.com>
2020-01-22 15:26:03 +0500

    dialogWorking Without designing

--- src/app/views/pages/try/try.component.ts
+++ src/app/views/pages/try/try.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material'
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { PopupWindowComponent } from '../popup-window/popup-window.component';
 
 @Component({
